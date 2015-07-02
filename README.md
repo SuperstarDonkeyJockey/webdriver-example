0694fc00d1ced2338c082ffeccb3e6cceb2f59ec
superstardonkeyjockey <waynepeacock@timeout.com>
2015-07-02 16:43:41 +0100

    first draft simple test

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # webdriver-example
+
+## One-off Setup
+1. Download the Chrome Driver from https://sites.google.com/a/chromium.org/chromedriver/downloads
+2. Create a folder called "WebDriver" in the root of your C drive.
+3. Copy the chromedriver.exe file to the C:\WebDriver you have just created.
+4. You are now ready to run the tests.
---
+++ TestProjectExample/ExampleTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+namespace WebDriverExample
+{
+    [TestClass]
+    public class WebDriverTests
+    {
+        IWebDriver driver;
+
+        By searchQuery = By.Name("q");
+        By searchSubmit = By.Name("btnG");
+        By searchResults = By.CssSelector("#ires");
+        By searchResultLinks = By.CssSelector("#ires .r a");
+
+        public void waitForElement(By by, int seconds = 5)
+        {
+            new WebDriverWait(driver, TimeSpan.FromSeconds(seconds)).Until(ExpectedConditions.ElementExists(by));
+        }
+
+        [TestInitialize]
+        public void StartDriver()
+        {
+            driver = new ChromeDriver(@"C:\WebDriver");
+        }
+
+        [TestCleanup]
+        public void CloseDriver()
+        {
+            driver.Close();
+        }
+
+        [TestMethod]
+        public void TopResultForGoogleSearch()
+        {
+            driver.Navigate().GoToUrl("https://www.google.co.uk");
+            driver.FindElement(searchQuery).SendKeys("Wayne Peacock");
+            driver.FindElement(searchSubmit).Click();
+            waitForElement(searchResults);
+            var resultLinks = driver.FindElements(searchResultLinks);
+            Assert.AreEqual("Wayne Peacock profiles - United Kingdom | LinkedIn", resultLinks[0].Text);
+        }
+    }
+}
